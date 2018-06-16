317443585b2a30296c06a199b819f5e3e394fb0d
Lenin Meza <merolhack@gmail.com>
2018-06-16 15:15:43 -0500

    Footer added

--- src/main.js
+++ src/main.js
@@ -8,34 +8,35 @@ import content from './html/content.html';
 import description1 from './html/description1.html';
 import description2 from './html/description2.html';
 import description3 from './html/description3.html';
+import footer from './html/footer.html';
 
 // Show the content methods
 window.showIndex = function() {
     // Create and append the HTML to body
     const root = document.createElement("div");
     root.className = 'content-fluid';
-    root.innerHTML = header + content;
+    root.innerHTML = header + content + footer;
     document.body.innerHTML = root.innerHTML;
 };
 window.showDescription1 = function() {
     // Create and append the HTML to body
     const root = document.createElement("div");
     root.className = 'content-fluid';
-    root.innerHTML = header + description1;
+    root.innerHTML = header + description1 + footer;
     document.body.innerHTML = root.innerHTML;
 };
 window.showDescription2 = function() {
     // Create and append the HTML to body
     const root = document.createElement("div");
     root.className = 'content-fluid';
-    root.innerHTML = header + description2;
+    root.innerHTML = header + description2 + footer;
     document.body.innerHTML = root.innerHTML;
 };
 window.showDescription3 = function() {
     // Create and append the HTML to body
     const root = document.createElement("div");
     root.className = 'content-fluid';
-    root.innerHTML = header + description3;
+    root.innerHTML = header + description3+ footer;
     document.body.innerHTML = root.innerHTML;
 };
 
